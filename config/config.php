825b1f8eb6d9159bf6cfd4a21f855730ac519b7f
Rakshit Patel <rakshitbharatproject@gmail.com>
2020-02-27 17:12:22 +0530

    - bug fixes

--- config/config.php
+++ config/config.php
@@ -7,8 +7,10 @@ return [
     'cleaning_commands' => [
         'composer dump-autoload',
         'php artisan config:cache',
+        'php artisan config:clear',
         'php artisan route:clear',
         'php artisan view:clear',
         'php artisan clear-compiled',
+        'php artisan down',
     ]
 ];
--- src/Cleanify.php
+++ src/Cleanify.php
@@ -7,16 +7,8 @@ class Cleanify
     public function cleanify()
     {
         $cleaning_commands = config('cleanify.cleaning_commands');
-        echo 'Command Started';
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $phpPath = exec("where php");
-        } else {
-            $phpPath = exec("which php");
-        }
-
         foreach ($cleaning_commands as $cleaning_commandss) {
-            $command = $phpPath . ' ' . base_path() . DIRECTORY_SEPARATOR . "artisan " . $cleaning_commandss;
-            echo shell_exec($command) . '\n';
+            echo shell_exec($cleaning_commandss);
         }
     }
 }
